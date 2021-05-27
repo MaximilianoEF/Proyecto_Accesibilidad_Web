46bd4d40f0a60b2c6896e45029f020e3c5fc4869
Maximiliano Franco <maxi22219@gmail.com>
2021-05-27 13:42:06 -0300

    PAW v1.0, tecnicas de tipo H y tipo C desarrolladas bajo las recomendaciones de la W3C

---
+++ Accesibilidad-Web/src/main/java/com/PAW/Tool/AccesibilidadWebApplication.java
@@ -0,0 +1,13 @@
+package com.PAW.Tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesibilidadWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesibilidadWebApplication.class, args);
+	}
+
+}
