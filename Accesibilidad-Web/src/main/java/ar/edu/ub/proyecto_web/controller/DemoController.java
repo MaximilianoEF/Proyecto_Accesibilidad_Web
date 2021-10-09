a331510edd4f441d1b71f197b21b22d0b2a3ba94
Pablo Hernan Rechimon <pablo.rechimon@gmail.com>
2021-10-09 02:00:10 -0300

    DriverControlle new update

--- Accesibilidad-Web/src/main/java/ar/edu/ub/proyecto_web/controller/DemoController.java
+++ Accesibilidad-Web/src/main/java/ar/edu/ub/proyecto_web/controller/DemoController.java
@@ -28,19 +28,70 @@ public class DemoController {
 	public String getResultadoAccesibilidadWeb() {
 		LinkedList<Principio> principios = new LinkedList<Principio>();
 		
+		// Principios
 		Principio pr1 = new Principio("1", "Perceptibilidad");
 		Principio pr2 = new Principio("1", "Operabilidad");
 		Principio pr3 = new Principio("1", "Comprensibilidad");
 		Principio pr4 = new Principio("1", "Robustez");
+		
+		// Pautas 1.x
 		Pauta pa11 = new Pauta("1.1", "Alternativas Textuales");
 		Pauta pa12 = new Pauta("1.2", "Contenido Multimedia dependiente del tiempo");
 		Pauta pa13 = new Pauta("1.3", "Adaptabilidad");
 		Pauta pa14 = new Pauta("1.4", "Distinguible");
-		Criterio c111 = new Criterio("1.1.1", "Contenido no textual");
-		Criterio c121 = new Criterio("1.2.1", "Solo audio y solo video");
+		// Pautas 2.x
+		Pauta pa21 = new Pauta("2.1", "Accesible a través del teclado");
+		Pauta pa22 = new Pauta("2.2", "Tiempo Suficiente");
+		Pauta pa23 = new Pauta("2.3", "Ataques");
+		Pauta pa24 = new Pauta("2.4", "Navegable");
+		// Pautas 3.x
+		Pauta pa31 = new Pauta("3.1", "Legible");
+		Pauta pa32 = new Pauta("3.2", "Predecible");
+		Pauta pa33 = new Pauta("3.3", "Ayuda a la entrada de datos");
+		// Pautas 4.x		
+		Pauta pa41 = new Pauta("4.1", "Compatible");
 		
-//		Criterio c113 = new Criterio("1.3.1", "Contenido no textual");
-//		Criterio c11N = new Criterio("1.1.N", "Contenido no textual");
+		// Criterios 
+		Criterio c111 = new Criterio("1.1.1", "Contenido no textual");
+		Criterio c121 = new Criterio("1.2.1", "Solo audio y solo video (Pregrabados)");
+		Criterio c122 = new Criterio("1.2.2", "Subtitulos (Pregrabados)");
+		Criterio c123 = new Criterio("1.2.3", "Audiodescripción o alternativa múltimedia (Pregrabada)");
+		Criterio c124 = new Criterio("1.2.4", "Subtitulos (Directo)");
+		Criterio c125 = new Criterio("1.2.5", "Audriodescripción (Pregrabada)");
+		Criterio c131 = new Criterio("1.3.1", "Información y relaciones");
+		Criterio c132 = new Criterio("1.3.2", "Secuencia significativa");
+		Criterio c133 = new Criterio("1.3.3", "Caracteristicas sensoriales");
+		Criterio c141 = new Criterio("1.4.1", "Empleo del color");
+		Criterio c142 = new Criterio("1.4.2", "Control del audio");
+		Criterio c143 = new Criterio("1.4.3", "Minimo");
+		Criterio c144 = new Criterio("1.4.4", "Cambio de tamaño del texto");
+		Criterio c145 = new Criterio("1.4.5", "Imagenes de Texto");
+		Criterio c211 = new Criterio("2.1.1", "Teclado");
+		Criterio c212 = new Criterio("2.1.2", "Sin trampa de teclado");
+		Criterio c221 = new Criterio("2.2.1", "Limite de tiempo ajustable");
+		Criterio c222 = new Criterio("2.2.2", "Pausar, detener, ocultar");
+		Criterio c231 = new Criterio("2.3.1", "Tres destellos o por debajo del umbral");
+		Criterio c241 = new Criterio("2.4.1", "Saltar bloques");
+		Criterio c242 = new Criterio("2.4.2", "Pagina titulada");
+		Criterio c243 = new Criterio("2.4.3", "Orden de foco");
+		Criterio c244 = new Criterio("2.4.4", "Proposito de un vinculo (En su contexto)");
+		Criterio c245 = new Criterio("2.4.5", "Multiples medios");
+		Criterio c246 = new Criterio("2.4.6", "Encabezados y etiquetas");
+		Criterio c247 = new Criterio("2.4.7", "Foco visible");
+		Criterio c311 = new Criterio("3.1.1", "Idioma de la pagina");
+		Criterio c312 = new Criterio("3.1.2", "Idioma de partes");
+		Criterio c321 = new Criterio("3.2.1", "Con foco");
+		Criterio c322 = new Criterio("3.2.2", "Con entrada y datos");
+		Criterio c324 = new Criterio("3.2.4", "Identificación consistente");
+		Criterio c331 = new Criterio("3.3.1", "Identificación de errores");
+		Criterio c332 = new Criterio("3.3.2", "Instrucciones o etiquetas");
+		Criterio c333 = new Criterio("3.3.3", "Sugerencia tras error");
+		Criterio c334 = new Criterio("3.3.4", "Prevención de errores (egales, financieros, de datos)");
+		Criterio c411 = new Criterio("4.1.1", "Interpretación");
+		Criterio c412 = new Criterio("4.1.2", "Nombre, rol, valor");
+		
+		//Tecnicas H
+		Tecnica h2 = new Tecnica("H2", "Combinando enlaces de texto e imagenes adyacentes para e mismo recurso");
 		
 		//Agrego Tecnica a Criterio 1.1.1
 		c111.agregarTecnica(new Tecnica("H2", "Jirafa"));
